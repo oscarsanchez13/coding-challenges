9c2d72301a8cf2f146ab9afd09a225a5f5ce311e
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-01 01:32:09 -0500

    Day 17 Challenge 1

--- Codewars/7-kyu/simple-string-characters.js
+++ Codewars/7-kyu/simple-string-characters.js
@@ -10,6 +10,43 @@ Solve("*'&ABCDabcde12345") = [4,5,5,3].
 special characters.
 */
 
+// Solution 1
 function solve(s){
-    //..
+    let result = [0,0,0,0];
+    let check = s.split("");
+     for(let i=0; i<check.length; i++){
+       if(isNaN(check[i]) === false){
+         result[2]++;
+       } 
+       else if(check[i] === check[i].toUpperCase() && check[i] !== check[i].toLowerCase()){
+         result[0]++;
+       }
+       else if(check[i] === check[i].toLowerCase() && check[i] !== check[i].toUpperCase()){
+         result[1]++;
+       } 
+       else {
+         result[3]++;
+       }
+     }
+     return result;
+}
+
+// Solution 2
+function solve(s){
+    let result = [0,0,0,0];
+    let check = s.split("");
+     for(let i=0; i<check.length; i++){
+       if(check[i].match(/[A-Z]/)){
+         result[0]++;
+       } 
+       else if(check[i].match(/[a-z]/)){
+         result[1]++;
+       }
+       else if(check[i].match(/[0-9]/)){
+         result[2]++;
+       } else {
+         result[3]++;
+       }
+     }
+     return result;
 }
