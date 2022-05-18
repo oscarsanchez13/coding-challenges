4466333db89709eaa36c322578af16c287f16bb3
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-18 00:04:44 -0500

    Day 26 Challenge 1

--- Codewars/6-kyu/create-phone-number.js
+++ Codewars/6-kyu/create-phone-number.js
@@ -13,6 +13,16 @@ this challenge.
 Don't forget the space after the closing parentheses!
 */
 
+// Solution 1
+function createPhoneNumber(n){
+    return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
+}
+
+// Solution 2
 function createPhoneNumber(numbers){
-  
+    var format = "(xxx) xxx-xxxx";    
+    for(var i = 0; i < numbers.length; i++){
+      format = format.replace('x', numbers[i]);
+    }    
+    return format;
 }
