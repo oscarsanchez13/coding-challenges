cf876c3c01b02647a20023d7c528dcfb7284eec7
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-18 00:53:19 -0500

    Day 26 Challenge 2

--- Codewars/6-kyu/who-likes-it.js
+++ Codewars/6-kyu/who-likes-it.js
@@ -18,6 +18,13 @@ text as shown in the examples:
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
 
+// Solution 1
 function likes(names) {
-    // TODO
+    switch(names.length) {
+        case 0: return 'no one likes this'; break;
+        case 1: return `${names[0]} likes this`; break;
+        case 2: return `${names[0]} and ${names[1]} like this`; break;
+        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`; break;
+        default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
 }
