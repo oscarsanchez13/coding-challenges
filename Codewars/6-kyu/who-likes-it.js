65113a91cf9dd9702e8dea96e81545d300a2197c
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-18 00:53:52 -0500

    Day 26 Challenge 2 Solution 2

--- Codewars/6-kyu/who-likes-it.js
+++ Codewars/6-kyu/who-likes-it.js
@@ -28,3 +28,14 @@ function likes(names) {
         default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
     }
 }
+
+// Solution 2
+function likes(names) {
+    return {
+      0: 'no one likes this',
+      1: `${names[0]} likes this`, 
+      2: `${names[0]} and ${names[1]} like this`, 
+      3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+      4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+    }[Math.min(4, names.length)]
+}
