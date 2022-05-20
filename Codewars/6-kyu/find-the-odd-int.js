e7e3aba4474090628bbba7fc02607aa0cab0fa3a
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-20 00:26:10 -0500

    Day 29 Challenge 2

--- Codewars/6-kyu/find-the-odd-int.js
+++ Codewars/6-kyu/find-the-odd-int.js
@@ -17,3 +17,11 @@ Examples
 1 time (which is odd).
 */
 
+function findOdd(A) {
+    for (let i = 0; i < A.length; i++) {
+      const count = A.filter(value => value === A[i]).length;
+      if (count % 2 == 1) {
+          return A[i];
+      }
+    }
+}
