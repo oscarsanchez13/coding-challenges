630c6b79011d5ccdd7bd71aea120ebbe73d4dacd
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-24 23:36:04 -0500

    Day 32 Solution 1

---
+++ Codewars/7-kyu/get-middle-character.js
@@ -0,0 +1,23 @@
+/*
+Description:
+
+You are going to be given a word. Your job is to return the 
+middle character of the word. If the word's length is odd, 
+return the middle character. If the word's length is even, 
+return the middle 2 characters.
+
+#Examples:
+
+getMiddle("test") should return "es"
+
+getMiddle("testing") should return "t"
+
+getMiddle("middle") should return "dd"
+
+getMiddle("A") should return "A"
+*/
+
+// Solution 1
+function getMiddle(s){
+    return s.length % 2 ? s[parseInt(s.length/2)] : s[(s.length/2) - 1] + s[(s.length/2)];
+}
