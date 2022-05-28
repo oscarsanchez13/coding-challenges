8167397f534692f509924cb6045429bdf399131a
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-05-27 23:20:55 -0500

    Day 33 Challenge 2 Solution 2

--- Codewars/6-kyu/is-a-number-prime.js
+++ Codewars/6-kyu/is-a-number-prime.js
@@ -32,3 +32,13 @@ function isPrime(n) {
 }
 
 // Solution 2
+function isPrime(num) {
+    if (num < 2) return false;
+    const limit = Math.sqrt(num);
+    for (let i = 2; i <= limit; ++i) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+}
