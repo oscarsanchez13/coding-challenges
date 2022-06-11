243ab261408c84d208fe82869b782326c45a8917
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-06-11 01:32:40 -0500

    Day 29 Challenge 1

--- Codewars/5-kyu/simple-pig-latin.js
+++ Codewars/5-kyu/simple-pig-latin.js
@@ -12,3 +12,30 @@ pigIt('Hello world !');     // elloHay orldway !
 function pigIt(str){
     //Code here
 }
+
+// Solution 1
+function pigIt(str){
+    let strArr = str.split(' ');
+    let pigLatin = [];
+  
+    for(let word of strArr){
+      if((/([a-zA-Z])/).test(word)){
+        pigLatin.push(word.substring(1) + word[0] + "ay");
+      }else{
+        pigLatin.push(word);
+      }
+    }
+    return pigLatin.join(" ");
+}
+
+// Solution 2 (It doesnt work with puctuation)
+function pigIt(str){
+    let arr = str.split(' ');
+    let newStr = [];
+    for(let i=0; i<arr.length; i++){
+      let word = arr[i];
+      let newWord = `${word.substring(1)}${word[0]}ay`;
+      newStr.push(newWord);
+    }
+    return newStr.join(' ');
+  }
