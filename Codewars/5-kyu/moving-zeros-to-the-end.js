81136921251e920c793d4890b9e6baa0e5324efe
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-06-11 18:46:20 -0500

    Day 30 Problem 2

--- Codewars/5-kyu/moving-zeros-to-the-end.js
+++ Codewars/5-kyu/moving-zeros-to-the-end.js
@@ -8,6 +8,26 @@ moveZeros([false,1,0,1,2,0,1,3,"a"])
 ^returns[false,1,1,2,1,3,"a",0,0]
 */
 
+// Solution 1 (Accessing all values in an array)
 function moveZeros(arr) {
-    return
+    let noZeros = arr.filter(x => x !== 0);
+    let zeros = arr.filter(x => x === 0);
+    return [...noZeros, ...zeros];
+}
+
+// Solution 1 Shortened
+let moveZeros = (arr) => {
+    return [...(arr.filter(x => x !== 0)), ...(arr.filter(x => x === 0))];
+}
+
+// Solution 2 (Using Concat method)
+function moveZeros(arr) {
+    let noZeros = arr.filter(x => x !== 0);
+    let zeros = arr.filter(x => x === 0);
+    return noZeros.concat(zeros);
+}
+
+// Solution 2 Shortened
+let moveZeros = (arr) => {
+    return arr.filter(x => x !== 0).concat(arr.filter(x => x === 0));
 }
