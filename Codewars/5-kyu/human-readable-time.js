094ded3848e704948bf47cc4d76f184860e307b9
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-06-13 00:52:24 -0500

    Day 30 Problem 2 Solution 1

--- Codewars/5-kyu/human-readable-time.js
+++ Codewars/5-kyu/human-readable-time.js
@@ -13,6 +13,15 @@ The maximum time never exceeds 359999 (99:59:59)
 You can find some examples in the test fixtures.
 */
 
-function humanReadable (seconds) {
-    return '';
+// Solution 1
+function humanReadable(x){
+    let hh = Math.floor(x / 3600);
+    let mm = Math.floor((((x % 31536000) % 86400) % 3600) / 60);
+    let ss = (((x % 31536000) % 86400) % 3600) % 60;
+    ss > 9 ? ss = '' + ss : ss = '0' + ss;
+    mm > 9 ? mm = '' + mm : mm = '0' + mm;
+    hh > 9 ? hh = '' + hh : hh = '0' + hh;
+    return `${hh}:${mm}:${ss}`;
 }
+
+// Solution 2
