507e91a889caecddeba476bd4d271b398e9f64df
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-06-17 20:54:00 -0500

    Day 32 setup

--- Codewars/5-kyu/rgb-to-hex.js
+++ Codewars/5-kyu/rgb-to-hex.js
@@ -18,6 +18,31 @@ rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3
 */
 
+// Solution 1 (BROKEN)
 function rgb(r, g, b){
-    // complete this function. tbc. 
+    let result = '';
+    let arr = [r,g,b];
+    for(let i=0; i<arr.length; i++){
+      function convertFirst(arr[i]) {
+        switch (Math.floor(arr[i] / 16)) {
+          case 10: result.push('A');break;
+          case 11: result.push('B');break;
+          case 12: result.push('C');break;
+          case 13: result.push('D');break;
+          case 14: result.push('E');break;
+          case 15: result.push('F');break;
+          default: result.push(Math.floor(((arr[i] / 16)+'')));
+        }
+       switch (Math.floor((((arr[i]/16) - 16) * 16))) {
+          case 10: result.push('A');break;
+          case 11: result.push('B');break;
+          case 12: result.push('C');break;
+          case 13: result.push('D');break;
+          case 14: result.push('E');break;
+          case 15: result.push('F');break;
+          default: result.push(Math.floor((((arr[i]/16) - 16) * 16)+''));
+        }
+      }
+    }
+    return result;
 }
