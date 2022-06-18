dd7b0e65a7e09d17307d10f2222b2b1396916d40
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-06-18 18:21:11 -0500

    Day 32 Challenge 1

--- Codewars/5-kyu/rgb-to-hex.js
+++ Codewars/5-kyu/rgb-to-hex.js
@@ -20,29 +20,41 @@ rgb(148, 0, 211) // returns 9400D3
 
 // Solution 1 (BROKEN)
 function rgb(r, g, b){
-    let result = '';
-    let arr = [r,g,b];
-    for(let i=0; i<arr.length; i++){
-      function convertFirst(arr[i]) {
-        switch (Math.floor(arr[i] / 16)) {
-          case 10: result.push('A');break;
-          case 11: result.push('B');break;
-          case 12: result.push('C');break;
-          case 13: result.push('D');break;
-          case 14: result.push('E');break;
-          case 15: result.push('F');break;
-          default: result.push(Math.floor(((arr[i] / 16)+'')));
-        }
-       switch (Math.floor((((arr[i]/16) - 16) * 16))) {
-          case 10: result.push('A');break;
-          case 11: result.push('B');break;
-          case 12: result.push('C');break;
-          case 13: result.push('D');break;
-          case 14: result.push('E');break;
-          case 15: result.push('F');break;
-          default: result.push(Math.floor((((arr[i]/16) - 16) * 16)+''));
-        }
-      }
+let result = '';
+let arr = [r,g,b];
+for(let i=0; i<arr.length; i++) {
+    switch (Math.floor(arr[i]/16)) {
+    case 10: result += 'A';break;
+    case 11: result += 'B';break;
+    case 12: result += 'C';break;
+    case 13: result += 'D';break;
+    case 14: result += 'E';break;
+    case 15: result += 'F';break;
+    default: result += (Math.floor(arr[i]/16) + '');
     }
-    return result;
+    switch (((arr[i]/16)-16)*16) {
+    case 10: result += 'A';break;
+    case 11: result += 'B';break;
+    case 12: result += 'C';break;
+    case 13: result += 'D';break;
+    case 14: result += 'E';break;
+    case 15: result += 'F';break;
+    default: result += ((((arr[i]/16)-16)*16) + '');
+    }
+}
+return result;
+}
+
+// Solution 2 (using toSting(16))
+function rgb(r, g, b) {
+    function convert(x){
+      if (x > 255) return 'FF';
+      else if (x < 0) return '00';
+      else 
+        return x
+                .toString(16)
+                .padStart(2,'0')
+                .toUpperCase();
+    }
+    return convert(r) + convert(g) + convert(b);
 }
