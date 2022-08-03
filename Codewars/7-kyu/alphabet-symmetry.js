33bf92326571ba93245345ba2bf8b5ac435ae39f
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-08-03 10:39:29 -0500

    Day 43 C1S2

--- Codewars/7-kyu/alphabet-symmetry.js
+++ Codewars/7-kyu/alphabet-symmetry.js
@@ -27,6 +27,20 @@ function solve(arr){
 };
 
 // Solution 2
-
+function solve(arr){  
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    
+    let final = []
+    for (let i = 0; i < arr.length; i++) {
+      let count = 0;
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j].toLowerCase() === alphabet[j]) {
+          count++;
+        }
+      }
+      final.push(count); 
+    }
+    return final
+};
 
 // Solution 3
