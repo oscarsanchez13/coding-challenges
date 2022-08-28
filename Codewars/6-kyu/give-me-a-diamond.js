10533801d63dcfc2ddb3bf2a06c1060aeb725022
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-08-27 23:57:27 -0500

    problem kyu 6

--- Codewars/6-kyu/give-me-a-diamond.js
+++ Codewars/6-kyu/give-me-a-diamond.js
@@ -36,9 +36,19 @@ A size 5 diamond:
 */
 
 // Solution 1
-function diamond(n){
-  
-    return diam;
+function diamond(n) {
+
+  if(n % 2 == 0 || n < 0) {
+      return null;
+  }
+
+  var middle = parseInt((n + 1) / 2);
+  var j, diam = '';
+  for(var i = 1; i <= n; i++) {
+      j = i <= middle ? i : n - i + 1;
+      diam += ' '.repeat(middle - j) + '*'.repeat(2 * j - 1) + '\n';
+  }
+  return diam;
 }
 
 // Solution 2
