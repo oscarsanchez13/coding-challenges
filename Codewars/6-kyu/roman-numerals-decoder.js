852298990f2ecd7ac31dd837700bd4a556c526e4
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-08-30 23:57:14 -0500

    C78S1 Refactored

--- Codewars/6-kyu/roman-numerals-decoder.js
+++ Codewars/6-kyu/roman-numerals-decoder.js
@@ -54,3 +54,24 @@ function solution (roman) {
     
     return total;
 }
+
+// Solution 1 Refactored
+function solution (roman) {
+    const conversion = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V": 5, "I":1}
+    
+    const array = roman.split('');
+    let total = 0;
+    let current, currentValue, next, nextValue;
+    
+    for(let i = 0; i < array.length; i++){
+      current = array[i];
+      currentValue = conversion[current]
+      
+      next = array[i+1];
+      nextValue = conversion[next]
+      
+      currentValue < nextValue ? total -= currentValue : total += currentValue;
+    }
+    
+    return total;
+}
