f019510df47c5ed15f31d9fe230a99bde3377776
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-08-30 23:53:12 -0500

    beautiful solution to kyu 6 problem 78

--- Codewars/6-kyu/roman-numerals-decoder.js
+++ Codewars/6-kyu/roman-numerals-decoder.js
@@ -28,3 +28,29 @@ M          1,000
 */
 
 // Solution 1
+function solution (roman) {
+    const conversion = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V": 5, "I":1}
+    
+    const array = roman.split('');
+    let total = 0;
+    let current;
+    let currentValue;
+    let next;
+    let nextValue;
+    
+    for(let i = 0; i < array.length; i++){
+      current = array[i];
+      currentValue = conversion[current]
+      
+      next = array[i+1];
+      nextValue = conversion[next]
+      
+      if(currentValue < nextValue){
+        total -= (currentValue);
+      } else {
+        total += (currentValue);
+      }
+    }
+    
+    return total;
+}
