ca431409fa408b378b1988fef6bdc36451a6a425
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-09-04 00:15:12 -0500

    c86s1&2

--- Codewars/8-kyu/area-of-a-square.js
+++ Codewars/8-kyu/area-of-a-square.js
@@ -8,3 +8,17 @@ the input. Return the result rounded to two decimals.
 Note: use the π value provided in your language 
 (Math::PI, M_PI, math.pi, etc)
 */
+
+// Solution 1
+function squareArea(A){
+    let R = (2*A/Math.PI) ** 2 
+    return +R.toFixed(2);
+}
+
+// Solution 2
+function squareArea(x){
+    let c = 4 * x;
+    let r = c / (2 * Math.PI);
+    let a = r ** 2;
+    return Number(a.toFixed(2));
+  }
