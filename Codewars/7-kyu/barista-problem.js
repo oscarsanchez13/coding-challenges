4879da3379c4c6b71cd4b888019fac5a0a7a53a3
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-09-06 23:14:28 -0500

    c84s2

--- Codewars/7-kyu/barista-problem.js
+++ Codewars/7-kyu/barista-problem.js
@@ -69,3 +69,16 @@ function barista(coffees){
     coffees.sort((a,b)=> a-b)
     return coffees.reduce((acc, cookTime, i) => acc + cookTime * (coffees.length-i) + 2*i, 0)
 }
+
+// Solution 2
+function barista(coffees){
+    let cleanTime = 2
+    let line = coffees.sort((a, b) => a - b)
+    let time = 0
+    while(line.length){
+        time += line[0] * line.length
+        line.shift()
+        time += cleanTime * line.length
+    }
+    return time
+}
