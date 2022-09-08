2a6007a157603ce1aeb2471251a39267162c7ed2
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-09-07 23:47:33 -0500

    Day87c85s1

---
+++ Codewars/7-kyu/binary-score.js
@@ -0,0 +1,20 @@
+/*
+Description:
+
+Objective
+Given a number n we will define its scORe to be 
+0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+
+Write a function that takes n and finds it's scORe.
+
+n	scORe n
+0	0
+1	1
+49	63
+1000000	1048575
+*/
+
+// Solution 1
+function score(n){
+    // Good Luck!
+}
