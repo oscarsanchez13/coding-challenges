bceb894e888713b96449b589f24fcfdc803310b3
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-09-10 21:44:54 -0500

    C82S2

--- Codewars/7-kyu/binary-score.js
+++ Codewars/7-kyu/binary-score.js
@@ -17,4 +17,16 @@ n	scORe n
 // Solution 1
 function score(n){
     return n > 0 ? parseInt(n.toString(2).replace(/0/g, 1), 2) : 0;
-} 
+}
+
+// Solution 2
+function score(n){
+    let i = 1;
+    
+    while (i < n) {
+      n |= i;
+      i = i << 1;
+    }
+    
+    return n;
+}
