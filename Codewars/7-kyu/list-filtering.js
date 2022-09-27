efe7ed8893fa75d7f7518d0dce5f9072cfa87824
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-09-26 23:29:13 -0500

    D100S2

--- Codewars/7-kyu/list-filtering.js
+++ Codewars/7-kyu/list-filtering.js
@@ -10,3 +10,9 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
+
+// Solution 1
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+}
+
