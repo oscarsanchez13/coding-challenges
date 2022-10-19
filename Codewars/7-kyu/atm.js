3d017f0d9e4232d8744155b304542f2bb89d0641
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2022-10-18 23:24:29 -0500

    D110 S1s2

--- Codewars/7-kyu/atm.js
+++ Codewars/7-kyu/atm.js
@@ -14,5 +14,13 @@ Return that number, or -1 if it is impossible.
 
 // Solution 1
 function solve(n) {
-    // Your code here
-  }
+  let count=0;
+  while(n>=500){count++;n-=500}
+  while(n>=200){count++;n-=200}
+  while(n>=100){count++;n-=100}
+  while(n>=50){count++;n-=50}
+  while(n>=20){count++;n-=20}
+  while(n>=10){count++;n-=10}
+  return n===0?count:-1
+}
+
