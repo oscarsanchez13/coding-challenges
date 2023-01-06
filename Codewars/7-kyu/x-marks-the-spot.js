d2d865857ffb1a047c2e1abea05852ed24105c53
architectsanchez13@gmail.com <architectsanchez13@gmail.com>
2023-01-05 23:52:59 -0600

    didn't like this ky s2

--- Codewars/7-kyu/x-marks-the-spot.js
+++ Codewars/7-kyu/x-marks-the-spot.js
@@ -56,3 +56,13 @@ function xMarksTheSpot(input){
   }
   return result[0];
 }
+
+// Solution 2
+const xMarksTheSpot = (input) => {
+  var arr=[];
+  for (var i=0; i<input.length; ++i)
+    for (var j=0; j<input[i].length; ++j)
+      if (input[i][j]=='x')
+        arr.push([i,j]);
+  return arr.length==1?arr[0]:[];
+}
